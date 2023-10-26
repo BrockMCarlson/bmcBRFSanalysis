1f98cf756cf18f66462f51297033d642b840a381
BrockMCarlson <brockmcarlson@gmail.com>
2023-10-26 13:47:25 -0500

    First result of example session for laminar stacked line plot!

--- laminarStackedPlot.m
+++ laminarStackedPlot.m
@@ -7,9 +7,9 @@ clear
 sessionLabel = '211008_B_bmcBRFS001';
 
 % Directories
-codeDir = 'C:\Users\Brock Carlson\Documents\GitHub\bmcBRFSanalysis'; 
+codeDir = 'C:\Users\neuropixel\Documents\GitHub\bmcBRFSanalysis'; 
 cd(codeDir)
-outputDir = 'C:\Users\Brock Carlson\Documents\MATLAB\FormattedDataOutputs';
+outputDir = 'C:\Users\neuropixel\Documents\MATLAB\formattedDataOutputs';
 
 cd(outputDir)
 load(strcat('sortedData_',sessionLabel,'.mat'))
@@ -20,30 +20,44 @@ probeLength = size(IDX(cond).LFP_bb{1,1},2);
 timeLength = length(STIM(1).sdftm);
 trlLength = size(IDX(cond).LFP_bb,1);
 xAxisTime = STIM(1).sdftm;
-idxPS = 12;
-idxNS = 11;
+idxps = 12;
+idxns = 11;
+granBtm = 24; % channel corresponding to the bottom of layer 4c
+
+%% Calculate V1 ch boundaries
+v1Top = granBtm - 9;
+v1Btm = granBtm + 5;
+v1Ch = v1Top:v1Btm;
+
 
 %% Create timetable
-% Organize matrix, (15ch,timeseries) for each stimulus
-PS = nan(15,1001);
-NS = nan(15,1001);
-
-% Create matrix of all trials (ch x time x trial)
-psTrlLength = size(IDX(12).correctTrialIndex,1);
+% Create matrix of all trials (time x ch x trial)
+% IDX(CONDITION).MUAe{TRIAL,TRIAL PERIOD}(TIME,CHANNEL)
+psTrlLength = size(IDX(idxps).correctTrialIndex,1);
 for psTrl = 1:psTrlLength
-    PS_CHxMSxTRL(:,:,psTrl) = IDX(idxPS).MUAe{psTrl,2}(:,:);
+    ps_msXchXtrl(:,:,psTrl) = IDX(idxps).MUAe{psTrl,2}(:,v1Ch); % MUA output is time x ch
+end
+nsTrlLength = size(IDX(idxns).correctTrialIndex,1);
+for nsTrl = 1:nsTrlLength
+    ns_msXchXtrl(:,:,nsTrl) = IDX(idxns).MUAe{nsTrl,2}(:,v1Ch); % MUA output is time x ch
 end
 %trl avg
-PS_avg = mean(PS_CHxMSxTRL,3);
+ps_avg = mean(ps_msXchXtrl,3);
+ps_table = array2table(ps_avg);
+ns_avg = mean(ns_msXchXtrl,3);
+ns_table = array2table(ns_avg);
 
-%Now convert PS_avg (a double array) into a table
+%Now convert ps_avg (a double array) into a table (input to timetable must
+%be a table)
 % The goal is to have 32 variables, each as a column, representing a
 % different depth. 
-youAreHere
+columnNames = {'sg_1','sg_2','sg_3','sg_4','sg_5','g_1','g_2','g_3','g_4','g_5','ig_1','ig_2','ig_3','ig_4','ig_5'};
+Time = milliseconds(-200):milliseconds(1):milliseconds(800);
+ps_TT = table2timetable(ps_table,'RowTimes',Time);
+ps_TT2 = renamevars(ps_TT,ps_TT.Properties.VariableNames,columnNames);
+ns_TT = table2timetable(ns_table,'RowTimes',Time);
+ns_TT2 = renamevars(ns_TT,ns_TT.Properties.VariableNames,columnNames);
 
-Time = [milliseconds(-200):milliseconds(1):milliseconds(800)];
-PS_TT = table2timetable(PS_table,'RowTimes',Time);
-NS_TT = table2timetable(NS_table,'RowTimes',Time);
 
 %% stackedplot()
-stackedplot(PS_TT,NS_TT)
+stackedplot(ps_TT2,ns_TT2)
