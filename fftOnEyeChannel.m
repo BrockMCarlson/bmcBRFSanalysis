b646322396541f3ccf86717e58644f28690c166e
BrockMCarlson <brockmcarlson@gmail.com>
2023-11-07 18:11:50 -0600

    Ayelet Landau and Daniele Re collaboration analysis update

--- fftOnEyeChannel.m
+++ fftOnEyeChannel.m
@@ -2,14 +2,14 @@
 %% Setup
 clear
 close all
-codeDir = 'C:\Users\neuropixel\Documents\GitHub\bmcBRFSanalysis'; 
+codeDir = 'C:\Users\Brock Carlson\Documents\GitHub\bmcBRFSanalysis'; 
 cd(codeDir)
-outputDir = 'C:\Users\neuropixel\Documents\MATLAB\formattedDataOutputs';
+sortedDataDir = 'D:\bmcBRFS_sortedData';
 
 
 
 %% Create dir loop
-cd(outputDir)
+cd(sortedDataDir)
 sortedDataFiles = dir('*sortedData*');
 close all
 dataFile = 1;
@@ -23,34 +23,45 @@ timeLength = length(sdftm);
 baselineTimeIndex = find(sdftm == -100):find(sdftm == 0); 
 xAxisTime = sdftm;
 
-%% Calculate FFT
-% In her original email, Ayelet wrote:
-% do you ever have a condition in which one stimulus to one eye onsets ...
-% earlier, before another stimulus to the other eye is presented ...
-% (so at the end both are on)?
+%% Demonstrate monocular preference
+% BRFS-like Congruent Adapted Flash is my condition of interest
+% IDX [5:8] 
+timeWindow = 1; % first 800 ms
+ms = 1:1000;
+ch = 3:17;
+for cond = 5:8
+    for trl = size(IDX(cond).MUAe,1)
+        monocular(cond,trl,ms,ch) = IDX(cond).MUAe{trl,timeWindow}(ms,ch);
+    end
+end
 
-% Conditions of interest: 12 vs 10
-% 12: 'BRFS IC Adapted Flash. NPO LeftEye adapting - PO RightEye flashed'
-% 10: 'BRFS IC Adapted Flash. PO LeftEye adapting - NPO RightEye flashed'
+You are here: figure out how to simply this system for not and in the future.
+4-D variables are untennable. size of monocular returns [1,9] ??
 
+monocularAvg = mean(thingToAverage,"all") ;   % end result should be 1001 x 32
+
+%% Plot response timecourse
 % Preferred MUA response is PO_RE for ch [3:17]
 
 prefFlash = IDX(12).MUAe;
 nullEye = IDX(10).MUAe;
 
 for i = 1:15
-    varForFFT_pref(:,i) = prefFlash{i,2}(:,3);
-    varForFFT_null(:,i) = nullEye{i,2}(:,3);
+    varForFFT_pref(:,i) = prefFlash{i,2}(:,3:17);
+    varForFFT_null(:,i) = nullEye{i,2}(:,3:17);
 end
-testFFT = fft(varForFFT_pref);
+
+
+%% Calculate FFT
+testFFT = fft(varForFFT_null);
 avgFFT = mean(testFFT,2);
 
 
 
-%% Continuous line plots for individual units
+%% plot FFT
 
 % Figure settings
-set(0,'DefaultFigureWindowStyle','normal')
+set(0,'DefaultFigureWindowStyle','docked')
 f = figure;
 set(f,'Position',[1000 74.3333 1.1197e+03 1.1633e+03])
 plot(abs(avgFFT))
